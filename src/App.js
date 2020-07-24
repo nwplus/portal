0c3eac193098ed81f5aabc866c0709e49517bec6
Ian Mah <ian.mah@hootsuite.com>
2020-07-24 12:00:29 -0700

    Connect firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import styled from 'styled-components'
+import React, { useState, useEffect } from 'react';
 import GlobalStyle from './theme/GlobalStyle'
 import ThemeProvider from './theme/ThemeProvider'
+import { db } from './utility/firebase'
 
 // temp for testing
 import ProgressBar from './components/ProgressBar'
 import TimeDisplay from './components/TimeDisplay'
 
-const ExampleText = styled.div`
-  color: ${p => p.theme.colors.text}
-`
 
 function App() {
+  const [hackathons, setHackathons] = useState()
+
+  // example getting all hackathons data
+  useEffect(() => {
+    db.collection('Hackathons').get().then(querySnapshot => {
+      const paths = querySnapshot.docs.map(doc => {
+        return doc.data()
+      })
+      setHackathons(paths)
+      console.log(paths)
+    })
+  }, [setHackathons])
+
   return (
     <>
       <ThemeProvider>
         <GlobalStyle />
-        <ExampleText>Live website.</ExampleText>
         <TimeDisplay days={1} hours={23} minutes={0} seconds={40}></TimeDisplay>
         <ProgressBar percent={33}></ProgressBar>
       </ThemeProvider>
