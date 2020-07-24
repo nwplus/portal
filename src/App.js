8ed2079e8e61eb153f45f718bc2f06691aa9752e
jackyzha0 <j.zhao2k19@gmail.com>
2020-07-23 23:49:37 -0700

    add countdown and progress bar

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import styled from 'styled-components'
 import GlobalStyle from './theme/GlobalStyle'
 import ThemeProvider from './theme/ThemeProvider'
 
+// temp for testing
+import ProgressBar from './components/ProgressBar'
+import TimeDisplay from './components/TimeDisplay'
+
 const ExampleText = styled.div`
-  color: ${p => p.theme.copy}
+  color: ${p => p.theme.text}
 `
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <ThemeProvider>
         <GlobalStyle />
         <ExampleText>Live website.</ExampleText>
+        <TimeDisplay days={1} hours={23} minutes={0} seconds={40}></TimeDisplay>
+        <ProgressBar current={33} total={100}></ProgressBar>
       </ThemeProvider>
-    </React.Fragment>
+    </>
   );
 }
 
