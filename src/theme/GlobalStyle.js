1e9fdddccb2d061675bdc1809f8a4a76c9f2b5d6
jackyzha0 <j.zhao2k19@gmail.com>
2020-07-31 00:17:40 -0700

    Add HK Grotesk

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -1,7 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
+import "./fonts/style.css"
  
 export default createGlobalStyle`
   body {
     margin: 0;
+    font-family: "HK Grotesk";
   }
 `;
