aae62b1320d071527d325f7c284e7e7c63fe2b5d
jackyzha0 <j.zhao2k19@gmail.com>
2020-09-05 08:59:30 -0700

    fix indentation in hackercountdown

--- src/containers/HackerCountdown.js
+++ src/containers/HackerCountdown.js
@@ -4,41 +4,41 @@ import { db } from '../utility/firebase'
 import { DB_COLLECTION, DB_HACKATHON } from '../utility/Constants'
 
 export default () => {
-    const [start, setStart] = useState(new Date())
-    const [end, setEnd] = useState(new Date())
+  const [start, setStart] = useState(new Date())
+  const [end, setEnd] = useState(new Date())
 
-    function toDateTime(secs) {
-        var t = new Date(1970, 0, 1);
-        t.setSeconds(secs);
-        return t;
-    }
+  function toDateTime(secs) {
+    var t = new Date(1970, 0, 1);
+    t.setSeconds(secs);
+    return t;
+  }
 
-    useEffect(() => {
-        const unsubscribe = db
-            .collection(DB_COLLECTION)
-            .doc(DB_HACKATHON)
-            .collection('DayOf')
-            .where('type', '==', 'hacking')
-            .onSnapshot(querySnapshot => {
-                const d = querySnapshot.docs[0].data()
-                if (d) {
-                    setStart(toDateTime(d.startTime.seconds))
-                    setEnd(toDateTime(d.endTime.seconds))
-                }
-            });
-        return unsubscribe
-    }, [setStart, setEnd])
+  useEffect(() => {
+    const unsubscribe = db
+      .collection(DB_COLLECTION)
+      .doc(DB_HACKATHON)
+      .collection('DayOf')
+      .where('type', '==', 'hacking')
+      .onSnapshot(querySnapshot => {
+        const d = querySnapshot.docs[0].data()
+        if (d) {
+          setStart(toDateTime(d.startTime.seconds))
+          setEnd(toDateTime(d.endTime.seconds))
+        }
+      });
+    return unsubscribe
+  }, [setStart, setEnd])
 
-    const eventDurationHours = (end.getTime() - start.getTime()) / 1000 / 3600
-    const beforeHackingStart = (new Date()).getTime() < start
-    const countDownDate = beforeHackingStart ? start : end
-    const eventName = beforeHackingStart ? "Hacking starts in..." : "Hacking ends in..."
+  const eventDurationHours = (end.getTime() - start.getTime()) / 1000 / 3600
+  const beforeHackingStart = (new Date()).getTime() < start
+  const countDownDate = beforeHackingStart ? start : end
+  const eventName = beforeHackingStart ? "Hacking starts in..." : "Hacking ends in..."
 
-    return (
-        <Countdown
-            countDownDate={countDownDate}
-            eventName={eventName}
-            eventDurationHours={eventDurationHours}
-        />
-    );
+  return (
+    <Countdown
+      countDownDate={countDownDate}
+      eventName={eventName}
+      eventDurationHours={eventDurationHours}
+    />
+  );
 };
