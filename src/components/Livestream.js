15ad2672d8e3eb5c896f9ddf2b8c6f7d891a4c4c
jackyzha0 <j.zhao2k19@gmail.com>
2020-09-19 16:27:45 -0700

    fix origins (again) and firebaserc

--- src/components/Livestream.js
+++ src/components/Livestream.js
@@ -55,7 +55,7 @@ const Livestream = () => {
     <Container>
       <TwitchContainer>
         <TwitchFrame
-          src="https://player.twitch.tv/?channel=nwplus&parent=localhost&parent=live.nwplus.io&parent=nwplus-ubc.firebaseapp.com"
+          src="https://player.twitch.tv/?channel=nwplus&parent=localhost&parent=live.nwplus.io&parent=nw-livesite.firebaseapp.com"
           frameborder="0"
           scrolling="no"
           allowfullscreen="true">
