f98fa45102bb48c1398b2a6a63709a74b7c20813
jackyzha0 <j.zhao2k19@gmail.com>
2020-10-12 18:29:55 -0700

    wip display schedule

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,19 +1,68 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Card } from './Common';
-import { P, A } from './Typography';
+import { P, H3 } from './Typography';
 
-export default ({ events }) => {
+const HOUR_HEIGHT = 150
+const EVENT_GAP = 10
 
-  function produceOptimalSchedule(events) {
+const ScheduleFlexContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: ${props => (props.duration * HOUR_HEIGHT) - 20}px;
+`
+
+const ScheduleColumn = styled.div`
+  flex: 0 0 400px;
+  height: auto;
+`
+
+const EventCard = styled(Card)`
+  position: absolute;
+  background-color: ${p => p.theme.colors.foreground};
+  margin: 5px;
+  padding: ${EVENT_GAP}px 15px;
+  width: 350px;
+  margin-top: ${props => (props.hourOffset * HOUR_HEIGHT)}px;
+  height: ${props => (props.duration * HOUR_HEIGHT) - (EVENT_GAP * 4)}px;
+  overflow-y: scroll;
+
+  &:after {
+    content  : "";
+    position : absolute;
+    z-index  : 1;
+    bottom   : 0;
+    left     : 0;
+    pointer-events   : none;
+    background-image : linear-gradient(to bottom, 
+                      rgba(255,255,255, 0), 
+                      ${p => p.theme.colors.foreground} 90%);
+    width    : 100%;
+    height   : 3em;
+  }
+`
+
+export default ({ events, hackathonStart, hackathonEnd }) => {
+  const produceOptimalSchedule = (events) => {
     const res = []
     let unusedEvents = events;
+
+    // build optimal columns list while there are still events that
+    // are not in the schedule
     while (unusedEvents.length > 0) {
       let latestTime = new Date(0) // epoch time lmao
       let usedIndices = []
+
+      // produce optimal schedule and track which events are used
       const sched = unusedEvents.reduce((accumulator, event, i) => {
         const curTime = new Date(event.startTime)
         if (curTime.getTime() >= latestTime.getTime()) {
+          const hoursFromStart = (curTime - hackathonStart) / 1000 / 60 / 60
+          event.hourOffset = hoursFromStart
+
+          const duration = ((new Date(event.endTime)) - (new Date(event.startTime))) / 1000 / 60 / 60
+          event.duration = duration
+
           accumulator.push(event)
           usedIndices.push(i)
           latestTime = new Date(event.endTime)
@@ -21,22 +70,43 @@ export default ({ events }) => {
         return accumulator;
       }, [])
 
-      // update unused
+      // update unused by removing used events
       unusedEvents = unusedEvents.filter((value, index) => {
         return usedIndices.indexOf(index) === -1;
       })
 
+      // add schedule to columns list
       res.push(sched)
     }
     return res
   }
 
-  console.log('____')
-  console.log(events)
-  console.log(produceOptimalSchedule(events))
+  const formatTime = (timeString) => {
+    const time = new Date(timeString);
+    const options = { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true };
+    return time.toLocaleDateString('en-US', options)
+  }
+
+  const schedule = produceOptimalSchedule(events)
+  console.log(schedule)
+
+  const durationOfHackathon = (hackathonEnd - hackathonStart) / 1000 / 60 / 60
 
   return (
     <Card>
+      <ScheduleFlexContainer duration={durationOfHackathon}>
+        {schedule.map((column) =>
+          <ScheduleColumn>
+            {column.map((event) =>
+              <EventCard hourOffset={event.hourOffset} duration={event.duration}>
+                <H3>{event.name}</H3>
+                <P>{formatTime(event.startTime)} - {formatTime(event.endTime)}</P>
+                <P>{event.description}</P>
+              </EventCard>
+            )}
+          </ScheduleColumn>
+        )}
+      </ScheduleFlexContainer>
     </Card>
   );
 }
--- src/containers/Schedule.js
+++ src/containers/Schedule.js
@@ -3,9 +3,24 @@ import Schedule from '../components/Schedule'
 import { H1 } from '../components/Typography';
 import { db } from '../utility/firebase'
 import { DB_COLLECTION, DB_HACKATHON, DAYOF_COLLECTION } from '../utility/Constants'
+import { livesiteDocRef } from '../utility/firebase'
 
 export default () => {
   const [events, setEvents] = useState([]);
+  const [start, setStart] = useState(new Date());
+  const [end, setEnd] = useState(new Date());
+
+  useEffect(() => {
+    const unsubscribe = livesiteDocRef
+      .onSnapshot(doc => {
+        const d = doc.data()
+        if (d) {
+          setStart(new Date(d.hackathonStart))
+          setEnd(new Date(d.hackathonEnd))
+        }
+      });
+    return unsubscribe
+  }, [setStart, setEnd])
 
   useEffect(() => {
     const unsubscribe = db
@@ -24,7 +39,11 @@ export default () => {
   return (
     <>
       <H1>Day-Of-Events Schedule</H1>
-      <Schedule events={events} />
+      <Schedule
+        events={events}
+        hackathonStart={start}
+        hackathonEnd={end}
+      />
     </>
   )
 };
