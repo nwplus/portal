448aa8e33808ad8e3900833ce222deeda5fde92c
Allison <acchiang98@gmail.com>
2020-11-04 23:24:49 -0800

    add chunkify

--- src/components/Common.js
+++ src/components/Common.js
@@ -87,3 +87,38 @@ export const DetailColumn = styled.ul`
 export const DetailAnswer = styled(P)`
   margin-bottom: 0.85em;
 `
+// given array, split into array of chunks of size n 
+// "balanced" (subarrays' lengths differ as less as possible) or 
+// "even" (all subarrays but the last have the same length):
+// https://stackoverflow.com/questions/8188548/splitting-a-js-array-into-n-arrays
+export const chunkify = ((a, n, balanced) => {
+  if (n < 2)
+    return [a];
+  var len = a.length,
+    out = [],
+    i = 0,
+    size;
+  if (len % n === 0) {
+    size = Math.floor(len / n);
+    while (i < len) {
+      out.push(a.slice(i, i += size));
+    }
+  }
+  else if (balanced) {
+    while (i < len) {
+      size = Math.ceil((len - i) / n--);
+      out.push(a.slice(i, i += size));
+    }
+  }
+  else {
+    n--;
+    size = Math.floor(len / n);
+    if (len % size === 0)
+      size--;
+    while (i < size * n) {
+      out.push(a.slice(i, i += size));
+    }
+    out.push(a.slice(size * n));
+  }
+  return out;
+});
