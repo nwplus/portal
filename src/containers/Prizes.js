f38adea7de9b6050be3fe611286181fe22196635
Allison <acchiang98@gmail.com>
2020-11-04 23:50:07 -0800

    dummy prizes for now

--- src/containers/Prizes.js
+++ src/containers/Prizes.js
@@ -6,6 +6,59 @@ const CenteredH1 = styled.h1`
 `
 
 
+const dummyMainPrizes = [
+  {
+    place: "1",
+    title: "1st Place",
+    content: "hello|hi|what's up"
+  },
+  {
+    place: "2",
+    title: "2nd Place",
+    content: "hello|hi|what's up"
+  },
+  {
+    place: "3",
+    title: "3rd Place",
+    content: "hello|hi|what's up",
+    main: true
+  }
+]
+
+const dummySponsorPrizes = [
+  {
+    place: false,
+    title: "sponsor prize 1",
+    content: "hello|hi|what's up"
+  },
+  {
+    place: false,
+    title: "sponsor prize 2",
+    content: "hello|hi|what's up"
+  },
+  {
+    place: false,
+    title: "sponsor prize 3",
+    content: "hello|hi|what's up"
+  },
+  {
+    place: false,
+    title: "sponsor prize 4",
+    content: "hello|hi|what's up"
+  },
+  {
+    place: false,
+    title: "sponsor prize 5",
+    content: "hello|hi|what's up"
+  },
+  {
+    place: false,
+    title: "sponsor prize 6",
+    content: "hello|hi|what's up"
+  }
+]
+
+
 export default () => {
 
   return (
