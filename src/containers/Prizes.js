24c5843cff914a647c30744b440a5d5131db091f
Allison <acchiang98@gmail.com>
2020-11-06 00:20:50 -0800

    new single query and dummy data

--- src/containers/Prizes.js
+++ src/containers/Prizes.js
@@ -3,13 +3,13 @@ import styled from 'styled-components'
 import { db } from '../utility/firebase'
 import { DB_COLLECTION, DB_HACKATHON } from '../utility/Constants'
 import { CardLike, DetailContainer, DetailColumn } from '../components/Common'
-import { H2, UL, LI, I } from '../components/Typography';
+import { H2, UL, LI, I } from '../components/Typography'
 import { chunkify } from '../utility/utilities'
 
 const CenteredH1 = styled.h1`
   text-align: center;
 `
-const COLUMNS_OF_PRIZES = 3;
+const COLUMNS_OF_PRIZES = 3
 
 const PrizeCard = styled.div`
   ${CardLike};
@@ -25,99 +25,137 @@ const getPrizes = () => {
     .get()
     .then(querySnapshot => {
       return querySnapshot.docs
-    });
+    })
 }
 
-const createPrizeList = (prizes) => {
+const createPrizeList = prizes => {
   return (
     <DetailContainer>
-      {
-        chunkify(prizes, COLUMNS_OF_PRIZES, true).map((chunk, i) =>
-          <DetailColumn key={i}>{chunk.map(singlePrize)}</DetailColumn>
-        )
-      }
+      {chunkify(prizes, COLUMNS_OF_PRIZES, true).map((chunk, i) => (
+        <DetailColumn key={i}>{chunk.map(singlePrize)}</DetailColumn>
+      ))}
     </DetailContainer>
-  );
+  )
 }
 
-
-const singlePrize = (prize) => {
+const singlePrize = prize => {
   return (
     <PrizeCard>
       <H2>{prize.title}</H2>
-      { (prize.sponsor === '') ? null : <I>{`Sponsored by ${prize.sponsor}`}</I>}
+      {prize.sponsor == null ? null : <I>{`Sponsored by ${prize.sponsor}`}</I>}
       <UL>
-        {prize.content.split("|").map((item) => <LI key={prize.title + item}>{item}</LI>)}
+        {prize.content.split('|').map(item => (
+          <LI key={prize.title + item}>{item}</LI>
+        ))}
       </UL>
     </PrizeCard>
-  );
+  )
 }
 
-
-export const MainPrizes = () => {
-  const [mainPrizes, setMainPrizes] = useState([]);
+export const AllPrizes = () => {
+  const [mainPrizes, setMainPrizes] = useState([])
+  const [sponsorPrizes, setSponsorPrizes] = useState([])
 
   useEffect(() => {
     getPrizes()
       .then(docs => {
-        // Only keep the main prizes
-        return Object.values(docs.reduce((result, doc) => {
-          const data = doc.data()
-          typeof data.place === "number" && result.push(data)
-          return result
-        }, []))
+        // let prizes;
+        // prizes.main = Object.values(docs.reduce((result, doc) => {
+        //   const data = doc.data()
+        //   typeof data.place === "number" && result.push(data)
+        //   return result
+        // }, []));
+        // prizes.sponsor = Object.values(docs.reduce((result, doc) => {
+        //   const data = doc.data()
+        //   !data.place && result.push(data)
+        //   return result
+        // }, []));
+        return {
+          main: [
+            {
+              place: '1',
+              title: '1st Place',
+              content: "hello|hi|what's up",
+            },
+            {
+              place: '2',
+              title: '2nd Place',
+              content: "hello|hi|what's up",
+            },
+            {
+              place: '3',
+              title: '3rd Place',
+              content: "hello|hi|what's up",
+            },
+          ],
+          sponsor: [
+            {
+              place: false,
+              title: 'sponsor prize 1',
+              content: "hello|hi|what's up",
+              sponsor: 'hi',
+            },
+            {
+              place: false,
+              title: 'sponsor prize 2',
+              content: "hello|hi|what's up",
+              sponsor: 'hi',
+            },
+            {
+              place: false,
+              title: 'sponsor prize 3',
+              content: "hello|hi|what's up",
+              sponsor: 'hi',
+            },
+            {
+              place: false,
+              title: 'sponsor prize 4',
+              content: "hello|hi|what's up",
+              sponsor: 'hi',
+            },
+            {
+              place: false,
+              title: 'sponsor prize 5',
+              content: "hello|hi|what's up",
+            },
+            {
+              place: false,
+              title: 'sponsor prize 6',
+              content: "hello|hi|what's up",
+            },
+          ],
+        }
       })
       .then(prizes => {
-        setMainPrizes(prizes);
+        setMainPrizes(prizes.main)
+        setSponsorPrizes(prizes.sponsor)
       })
-  }, [setMainPrizes]);
+  }, [setMainPrizes, setSponsorPrizes])
 
-
+  let res = []
   if (mainPrizes.length > 0) {
-    return (
+    res.push(
       <>
         <CenteredH1>Overall Prizes</CenteredH1>
-        { createPrizeList(mainPrizes)}
+        {createPrizeList(mainPrizes)}
       </>
-    );
+    )
   }
-  return null;
-}
-
-export const SponsorPrizes = () => {
-  const [sponsorPrizes, setSponsorPrizes] = useState([]);
-
-  useEffect(() => {
-    getPrizes()
-      .then(docs => {
-        // Only keep the sponsor prizes
-        return Object.values(docs.reduce((result, doc) => {
-          const data = doc.data()
-          !data.place && result.push(data)
-          return result
-        }, []))
-      })
-      .then(prizes => {
-        setSponsorPrizes(prizes);
-      })
-  }, [setSponsorPrizes]);
-
   if (sponsorPrizes.length > 0) {
-    return (
+    res.push(
       <>
         <CenteredH1>Sponsored Categories</CenteredH1>
-        { createPrizeList(sponsorPrizes)}
+        {createPrizeList(sponsorPrizes)}
       </>
-    );
+    )
   }
-  return null;
+  return res
 }
 
 export default () => {
   return (
     <>
-      <MainPrizes />
-      <SponsorPrizes />
+      <AllPrizes />
     </>
-  );
+  )
 }
