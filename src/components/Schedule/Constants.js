08e394b372c6d0c59149208bc0dba68798c25651
jackyzha0 <j.zhao2k19@gmail.com>
2020-11-15 20:47:49 -0800

    patch: workshop -> workshops

--- src/components/Schedule/Constants.js
+++ src/components/Schedule/Constants.js
@@ -5,6 +5,6 @@ export const EVENT_WIDTH = 400
 export const EVENT_TYPES = {
   main: { label: 'MAIN', colour: '#FFFFFF' },
   minievents: { label: 'ACTIVITY', colour: '#FFE27A' },
-  workshop: { label: 'WORKSHOP', colour: '#31E0E0' },
+  workshops: { label: 'WORKSHOP', colour: '#31E0E0' },
   notices: { label: 'NOTICE', colour: '#9EFF7C' },
 }
