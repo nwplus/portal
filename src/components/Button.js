8c493b57bb7b72f23c5a2d99394175781e9421b8
Allison <acchiang98@gmail.com>
2020-11-24 20:42:38 -0800

    revert to styled A component (not Button)

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,14 +11,14 @@ const buttonSize = {
 
 const buttonColors = ['primary', 'secondary', 'tertiary']
 
-const StyledButton = styled.button`
+const StyledButton = styled.a`
   display: block;
   text-decoration: none;
   font-family: 'HK Grotesk';
   font-weight: ${p => p.theme.typography.h2.weight};
   border: transparent;
   transition: all 250ms;
-  min-width: ${p => (p.size ? buttonSize[p.size] : buttonSize['default'])};
+  width: ${p => (p.size ? buttonSize[p.size] : buttonSize['default'])};
   text-align: center;
   padding: 0.75em;
   border-radius: 3px;
@@ -29,19 +29,29 @@ const StyledButton = styled.button`
   :focus {
     outline: 0;
   }
-  :disabled {
+  ${p =>
+    p.disabled &&
+    `
     cursor: not-allowed;
-    opacity: ${p => p.theme.opacity.disabled};
-  }
+    opacity: ${p.theme.opacity.disabled};
+    `}
   ${p =>
     (p.color === buttonColors[0] || !buttonColors.includes(p.color)) && // primary color (gradient button) or not specified color
     `color: ${p.theme.colors.background};
     background: ${p.theme.colors.primaryGradient};
     :hover {
+      ${
+        p.disabled
+          ? `
+      cursor: not-allowed;
+      `
+          : `
       background: ${p.theme.colors.primaryGradientHover};
+      `
+      }
     }
-    :disabled {
-      background: ${p.theme.colors.primaryGradient};
+    :focus {
+      box-shadow: 0 0 0 .2rem ${hexToRgba(p.theme.colors.primary, 0.5)};
     }
     ${
       p.theme.custom_imgs === 'hc' &&
@@ -50,9 +60,6 @@ const StyledButton = styled.button`
       background-size: contain;
       background-repeat: no-repeat;
       width: 140px;
-      :focus {
-        box-shadow: 0 0 0 .2rem ${hexToRgba(p.theme.colors.primary, 0.2)};
-      }
       `
     }
     `}
@@ -62,26 +69,43 @@ const StyledButton = styled.button`
     background: ${hexToRgba(p.theme.colors.primary, 0)};
     border: 1px solid ${p.theme.colors.primary};
     :hover {
+      ${
+        p.disabled
+          ? `
+      cursor: not-allowed;
+      `
+          : `
       background: ${hexToRgba(p.theme.colors.primary, 0.2)};
+      `
+      }
     }
     :focus {
       background: ${hexToRgba(p.theme.colors.primary, 0.5)};
-    }
-    :disabled {
-      background: ${hexToRgba(p.theme.colors.primary, 0)};
     }`}
   ${p =>
     p.color === buttonColors[2] && // tertiary color (solid button)
     `color: ${p.theme.colors.background};
     background: ${p.theme.colors.primary};
     :hover {
+      ${
+        p.disabled
+          ? `
+      cursor: not-allowed;
+      `
+          : `
       background: ${p.theme.colors.tertiaryHover};
+      `
+      }
     }
-    :disabled {
-      background: ${p.theme.colors.primary};
+    :focus {
+      box-shadow: 0 0 0 .2rem ${hexToRgba(p.theme.colors.primary, 0.5)};
     }`}
 `
 
-export const Button = props => <StyledButton {...props}>{props.children}</StyledButton>
+export const Button = props => (
+  <StyledButton tabIndex={props.disabled ? null : 0} {...props}>
+    {props.children}
+  </StyledButton>
+)
 
 export default Button
