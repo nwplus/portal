84fe7055a8dca44d45388d4631ef7597fcabeea9
Jenny Pan <panjenny0@gmail.com>
2020-11-26 14:50:50 -0800

    changed useLocation to Redirect

--- src/pages/Application/Form.js
+++ src/pages/Application/Form.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { useLocation } from 'wouter'
+import { Redirect } from 'wouter'
 import Part1 from '../../containers/Application/Part1'
 import Part2 from '../../containers/Application/Part2'
 import Part3 from '../../containers/Application/Part3'
 
 export default ({ part }) => {
-  const [location, setLocation] = useLocation()
-
   switch (part) {
     case 'part-1':
       return <Part1>Tell us about yourself!</Part1>
@@ -15,11 +13,6 @@ export default ({ part }) => {
     case 'part-3':
       return <Part3>Almost there</Part3>
     default:
-      return (
-        <div>
-          <h1>Uh-oh, we can't seem to find what you're looking for</h1>
-          <button onClick={() => setLocation('/login')}>Take me back to the login page</button>
-        </div>
-      )
+      return <Redirect to="/login" />
   }
 }
