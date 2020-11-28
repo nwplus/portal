b462e0ad0dd0c39c7bdf35866d3de2321bcb13ea
andykwan1124 <woody.andygrant@gmail.com>
2020-11-28 02:55:27 -0800

    added vert progress bar

---
+++ src/components/VerticalProgressBar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ProgressBarContainer = styled.div`
+  width: 11px;
+  height: ${p => p.vHeight || 50}vh;
+  border-radius: 17px;
+  background-color: ${p => p.theme.colors.default};
+  position: fixed;
+  top: 50%;
+  margin-top: ${p => (p.vHeight ? -p.vHeight / 2 : -25)}vh;
+`
+
+const ProgressBarFill = styled.div`
+  height: ${p => p.percent}%;
+  width: 100%;
+  background-color: ${p => p.theme.colors.primary};
+  border-radius: inherit;
+  transition: height 0.3s ease-in;
+`
+
+const VerticalProgressBar = ({ percent, vHeight }) => {
+  return (
+    <ProgressBarContainer vHeight={vHeight}>
+      <ProgressBarFill percent={percent} />
+    </ProgressBarContainer>
+  )
+}
+
+export default VerticalProgressBar
