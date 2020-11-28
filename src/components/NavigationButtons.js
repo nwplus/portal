d04a2e558f1ce38de4e66ea74699369735b4f5d8
Kevin Zou <kevin.zou@opencare.com>
2020-11-27 23:15:36 -0800

    Removing blank line

--- src/components/NavigationButtons.js
+++ src/components/NavigationButtons.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-
 import { Button } from './Button'
 import { screenBreakpoints } from './Common'
 import { I } from './Typography'
