2083864bcabcba8e0946c0eb3a2fe8cc2dbf65d8
Jenny Pan <panjenny0@gmail.com>
2020-11-28 13:27:05 -0800

    overflow not working sos

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -9,19 +9,16 @@ import { H1, P } from '../../components/Typography'
 import { CenterHorizontally } from '../../components/Common'
 
 const LandingContainer = styled.div`
-  position: fixed;
-  display: block;
+  width: 100vw;
+  height: 100vh;
   overflow-x: hidden;
-  margin: 0 auto;
-  // padding: 0 auto;
-  // z-index: -1;
-  color: white;
   ${p => !p.showFooter && 'overflow-y: hidden;'}
 `
 
 const HoloBackground = styled.img`
   position: absolute;
   float: left;
+  overflow-x: hidden;
   display: block;
   opacity: 0.8;
   width: 100%;
@@ -44,27 +41,25 @@ const StyledBanner = styled(Banner)`
     top: 18em;
     padding: 56px 24px 24px;
     text-align: center;
-    z-index: 1;
+    z-index: 0;
   }
 `
 
 // TODO: add sponsors if footer is shown
 export default ({ heading, description, showFooter, children }) => (
-  <>
+  <LandingContainer showFooter={showFooter}>
     <StyledLogoLockup src={logo_lockup} />
     <StyledBanner>
       <H1 size="1.5em">{heading}</H1>
       <P>{description}</P>
       {children}
     </StyledBanner>
-    <LandingContainer showFooter={showFooter}>
-      {showFooter ? (
-        <>
-          <Footer /> <HoloBackground src={holo_end} />
-        </>
-      ) : (
-        <HoloBackground src={holo} />
-      )}
-    </LandingContainer>
-  </>
+    {showFooter ? (
+      <>
+        <Footer /> <HoloBackground src={holo_end} />
+      </>
+    ) : (
+      <HoloBackground src={holo} />
+    )}
+  </LandingContainer>
 )
