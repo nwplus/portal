7bb0126cb062bb3aa03583f0c79cbb87e96b7e61
andykwan1124 <woody.andygrant@gmail.com>
2020-11-28 19:13:44 -0800

    removed containers

--- src/pages/Charcuterie.js
+++ src/pages/Charcuterie.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
 import { H1, H2, H3, P, A, QuestionHeading } from '../components/Typography'
 import { Card } from '../components/Common.js'
 import { Button } from '../components/Button'
@@ -13,16 +12,6 @@ import Checkbox from '../components/Checkbox'
 import Select from '../components/Select'
 import Dropdown from '../components/Dropdown'
 import VerticalProgressBar from '../components/VerticalProgressBar'
-
-const Container = styled.div`
-  display: flex;
-  column-gap: 50px;
-`
-
-const ColumnContainer = styled.div`
-  flex: 1 1 0;
-  min-width: 0;
-`
 
 const options = [
   { value: 'chocolate', label: 'Chocolatewerwerwheirwheifuhwieufhwieuhfiu' },
@@ -69,244 +58,237 @@ export default () => {
 
   return (
     <>
-      <Container>
-        <div>
-          <VerticalProgressBar percent={progress} />
-        </div>
-        <ColumnContainer>
-          <Button color="secondary" width="flex" href={`javascript:(${toggleTheme})()`}>
-            Toggle Theme
-          </Button>
-          <P>
-            Theme switcher. Drag the bookmarklet button from the page to your Bookmarks Toolbar. It
-            should appear on the toolbar
-          </P>
+      <Button color="secondary" width="flex" href={`javascript:(${toggleTheme})()`}>
+        Toggle Theme
+      </Button>
+      <P>
+        Theme switcher. Drag the bookmarklet button from the page to your Bookmarks Toolbar. It
+        should appear on the toolbar
+      </P>
 
-          <H1>Charcuturie</H1>
-          <>
-            <H1>This is an h1.</H1>
-            <H2>This is an h2.</H2>
-            <H3>This is an h3.</H3>
-            <P>
-              <A href="https://en.wikipedia.org/wiki/Charcuterie">Charcuterie</A> most often
-              consists of a variety of meats and cheeses, often paired with crackers, fruit, nuts,
-              and spreads. An ideal charcuterie board has a good balance of flavors and textures and
-              has foods that contrast and complement each other's taste. It's some really long text.
-              I'm really writing this way later than I should be. Is this what it's like to sell
-              your soul to nwPlus?{' '}
-            </P>
-            <br></br>
-            <QuestionHeading>this is a question heading</QuestionHeading>
-            <QuestionHeading>question 14</QuestionHeading>
-            <H1>How did you hear about nwHacks?</H1>
-          </>
-          <>
-            <H2>Countdown</H2>
-            <Countdown
-              countDownDate={new Date('Fri Aug 05 2020 00:01:22 GMT-0700 (Pacific Daylight Time)')}
-              eventDurationHours={48}
-              eventName="Hacking ends in..."
-            />
-          </>
-          <Card>
-            <H2>Card Element</H2>
-            <P>It can contain content. And even buttons!</P>
-            <P>Colors</P>
-            <Button color="primary">Primary</Button>
-            <Button color="primary" disabled>
-              Primary
-            </Button>
-            <Button color="secondary" width="flex">
-              Secondary
-            </Button>
-            <Button color="secondary" width="flex" disabled>
-              Secondary
-            </Button>
-            <Button color="tertiary">Tertiary</Button>
-            <Button color="tertiary" disabled>
-              Tertiary
-            </Button>
-            <P>Widths</P>
-            <Button width="small" color="secondary">
-              Small
-            </Button>
-            <Button color="secondary">Default</Button>
-            <Button width="flex" color="secondary">
-              Flex (ie. as wide as the label)
-            </Button>
-            <Button width="large" color="secondary">
-              Large
-            </Button>
-            <P>Heights</P>
-            <Button height="short" color="secondary">
-              Short
-            </Button>
-            <Button height="tall" color="secondary">
-              Tall
-            </Button>
-            <TextInput placeholder="Default" />
-            <TextInput value="With Value" />
-            <TextInput value="With Value Disabled" disabled />
-            <TextInput placeholder="Disabled" disabled />
-            <TextInput placeholder="Invalid" invalid errorMsg={'Pls try again lol'} />
-            <TextInput placeholder="Medium" size="medium" />
-            <TextInput placeholder="Large" size="large" />
-          </Card>
-          <TextArea
-            placeholder="TextArea with placeholder."
-            maxLength="10"
-            value={textAreaValue}
-            onChange={setTextAreaValue}
-          />
-          <Accordion heading="Accordion Component">
-            Some hidden content. This can get pretty long too, and even contain other stuff like
-            headers or images.
-          </Accordion>
-          <H2>Livestream Component</H2>
-          <Livestream />
-          <JudgingCard
-            title="Imposter"
-            imgUrl="https://img.youtube.com/vi/PQgHXPGoKwg/maxresdefault.jpg"
-            teamName="H4ckH0use"
-            buttonLabel="Judge this submission"
-            description="Imposter is a productivity timer designed to keep friends on task together even when working remotely. It aims to create a productive and social environment for all of us working from home."
-          />
-          <H2>Checkbox</H2>
-          <Checkbox
-            label="Default state"
-            checked={states.checkbox}
-            onChange={() => setStates({ ...states, checkbox: !states.checkbox })}
-          />
-          <Checkbox label="Selected state" checked readOnly />
-          <H2>Selects</H2>
-          <H3>Radio</H3>
-          <Select
-            type="radio"
-            name="radioSelect"
-            label="Default state"
-            checked={states.radio === 'default'}
-            onChange={e => setStates({ ...states, radio: e.target.value })}
-            value="default"
-          />
-          <Select
-            type="radio"
-            name="radioSelect"
-            label="Selected state"
-            checked={states.radio === 'selected'}
-            onChange={e => setStates({ ...states, radio: e.target.value })}
-            value="selected"
-          />
-          <Select type="radio" name="selects" label="Disabled state" disabled />
-          <H3>Multiselects (Select all that apply)</H3>
-          <Select
-            type="checkbox"
-            label="Option 1"
-            checked={states.multiselect.option1}
-            onChange={() =>
-              setStates({
-                ...states,
-                multiselect: { ...states.multiselect, option1: !states.multiselect.option1 },
-              })
-            }
-          />
-          <Select
-            type="checkbox"
-            label="Option 2"
-            checked={states.multiselect.option2}
-            onChange={() =>
-              setStates({
-                ...states,
-                multiselect: { ...states.multiselect, option2: !states.multiselect.option2 },
-              })
-            }
-          />
-          <Select type="checkbox" label="Selected state" checked readOnly />
-          <Select type="checkbox" label="Disabled state" disabled />
-          <H2>Dropdowns</H2>
-          <H3>Normal dropdown</H3>
-          <Dropdown
-            options={options}
-            placeholder={'I am a placeholder'}
-            isSearchable={false}
-            onChange={inputValue => console.log(inputValue)}
-            isValid
-          />
-          <H3>Searchable dropdown</H3>
-          <Dropdown
-            options={options}
-            placeholder={'im tired'}
-            isSearchable
-            formatCreateLabel={inputValue => `Cant find this!!! Use "${inputValue}" instead`}
-            onChange={inputValue => console.log(inputValue)}
-            emptySearchDefaultOption={'Start typing to search'}
-            noOptionsMessage={() => 'u messed up'}
-            canCreateNewOption={false}
-            isValid
-          />
-          <H3>Searchable and creatable dropdown</H3>
-          <Dropdown
-            options={options}
-            placeholder={'Hi I am a placeholder'}
-            isSearchable
-            formatCreateLabel={inputValue => `Cant find this!!! Use "${inputValue}" instead`}
-            onChange={inputValue => console.log(inputValue)}
-            emptySearchDefaultOption={'Start typing to search'}
-            noOptionsMessage={() => 'u messed up'}
-            canCreateNewOption
-            isValid
-          />
-          <H3>Invalid dropdown</H3>
-          <Dropdown
-            options={options}
-            placeholder={'im tired'}
-            isSearchable
-            formatCreateLabel={inputValue => `Cant find this!!! Use "${inputValue}" instead`}
-            onChange={inputValue => console.log(inputValue)}
-            emptySearchDefaultOption={'Start typing to search'}
-            noOptionsMessage={() => 'u messed up'}
-            canCreateNewOption={false}
-            isValid={false}
-            errorMessage={'Please select something!'}
-          />
-          <H3>Debounced dropdown</H3>
-          <Dropdown
-            options={options}
-            placeholder={'im tired'}
-            isSearchable
-            formatCreateLabel={inputValue => `Cant find this!!! Use "${inputValue}" instead`}
-            onChange={inputValue => console.log(inputValue)}
-            emptySearchDefaultOption={'Start typing to search'}
-            noOptionsMessage={() => 'u messed up'}
-            canCreateNewOption={false}
-            isValid
-            errorMessage={'Please select something!'}
-            debounceEnabled
-            throttleTime={1000}
-          />
-          <H3>Debounced creatable dropdown</H3>
-          <Dropdown
-            options={options}
-            placeholder={'im tired'}
-            isSearchable={true}
-            formatCreateLabel={inputValue => `Cant find this!!! Use "${inputValue}" instead`}
-            onChange={inputValue => console.log(inputValue)}
-            emptySearchDefaultOption={'Start typing to search'}
-            noOptionsMessage={() => 'u messed up'}
-            canCreateNewOption
-            isValid
-            errorMessage={'Please select something!'}
-            debounceEnabled
-            throttleTime={1000}
-          />
-          <H2>Change Progress Bar</H2>
-          <Button width="flex" onClick={() => progress < 100 && setProgress(progress + 10)}>
-            Increase!
-          </Button>
-          <Button width="flex" onClick={() => progress >= 10 && setProgress(progress - 10)}>
-            Decrease!
-          </Button>
-        </ColumnContainer>
-      </Container>
+      <H1>Charcuturie</H1>
+      <>
+        <H1>This is an h1.</H1>
+        <H2>This is an h2.</H2>
+        <H3>This is an h3.</H3>
+        <P>
+          <A href="https://en.wikipedia.org/wiki/Charcuterie">Charcuterie</A> most often consists of
+          a variety of meats and cheeses, often paired with crackers, fruit, nuts, and spreads. An
+          ideal charcuterie board has a good balance of flavors and textures and has foods that
+          contrast and complement each other's taste. It's some really long text. I'm really writing
+          this way later than I should be. Is this what it's like to sell your soul to nwPlus?{' '}
+        </P>
+        <br></br>
+        <QuestionHeading>this is a question heading</QuestionHeading>
+        <QuestionHeading>question 14</QuestionHeading>
+        <H1>How did you hear about nwHacks?</H1>
+      </>
+      <>
+        <H2>Countdown</H2>
+        <Countdown
+          countDownDate={new Date('Fri Aug 05 2020 00:01:22 GMT-0700 (Pacific Daylight Time)')}
+          eventDurationHours={48}
+          eventName="Hacking ends in..."
+        />
+      </>
+      <Card>
+        <H2>Card Element</H2>
+        <P>It can contain content. And even buttons!</P>
+        <P>Colors</P>
+        <Button color="primary">Primary</Button>
+        <Button color="primary" disabled>
+          Primary
+        </Button>
+        <Button color="secondary" width="flex">
+          Secondary
+        </Button>
+        <Button color="secondary" width="flex" disabled>
+          Secondary
+        </Button>
+        <Button color="tertiary">Tertiary</Button>
+        <Button color="tertiary" disabled>
+          Tertiary
+        </Button>
+        <P>Widths</P>
+        <Button width="small" color="secondary">
+          Small
+        </Button>
+        <Button color="secondary">Default</Button>
+        <Button width="flex" color="secondary">
+          Flex (ie. as wide as the label)
+        </Button>
+        <Button width="large" color="secondary">
+          Large
+        </Button>
+        <P>Heights</P>
+        <Button height="short" color="secondary">
+          Short
+        </Button>
+        <Button height="tall" color="secondary">
+          Tall
+        </Button>
+        <TextInput placeholder="Default" />
+        <TextInput value="With Value" />
+        <TextInput value="With Value Disabled" disabled />
+        <TextInput placeholder="Disabled" disabled />
+        <TextInput placeholder="Invalid" invalid errorMsg={'Pls try again lol'} />
+        <TextInput placeholder="Medium" size="medium" />
+        <TextInput placeholder="Large" size="large" />
+      </Card>
+      <TextArea
+        placeholder="TextArea with placeholder."
+        maxLength="10"
+        value={textAreaValue}
+        onChange={setTextAreaValue}
+      />
+      <Accordion heading="Accordion Component">
+        Some hidden content. This can get pretty long too, and even contain other stuff like headers
+        or images.
+      </Accordion>
+      <H2>Livestream Component</H2>
+      <Livestream />
+      <JudgingCard
+        title="Imposter"
+        imgUrl="https://img.youtube.com/vi/PQgHXPGoKwg/maxresdefault.jpg"
+        teamName="H4ckH0use"
+        buttonLabel="Judge this submission"
+        description="Imposter is a productivity timer designed to keep friends on task together even when working remotely. It aims to create a productive and social environment for all of us working from home."
+      />
+      <H2>Checkbox</H2>
+      <Checkbox
+        label="Default state"
+        checked={states.checkbox}
+        onChange={() => setStates({ ...states, checkbox: !states.checkbox })}
+      />
+      <Checkbox label="Selected state" checked readOnly />
+      <H2>Selects</H2>
+      <H3>Radio</H3>
+      <Select
+        type="radio"
+        name="radioSelect"
+        label="Default state"
+        checked={states.radio === 'default'}
+        onChange={e => setStates({ ...states, radio: e.target.value })}
+        value="default"
+      />
+      <Select
+        type="radio"
+        name="radioSelect"
+        label="Selected state"
+        checked={states.radio === 'selected'}
+        onChange={e => setStates({ ...states, radio: e.target.value })}
+        value="selected"
+      />
+      <Select type="radio" name="selects" label="Disabled state" disabled />
+      <H3>Multiselects (Select all that apply)</H3>
+      <Select
+        type="checkbox"
+        label="Option 1"
+        checked={states.multiselect.option1}
+        onChange={() =>
+          setStates({
+            ...states,
+            multiselect: { ...states.multiselect, option1: !states.multiselect.option1 },
+          })
+        }
+      />
+      <Select
+        type="checkbox"
+        label="Option 2"
+        checked={states.multiselect.option2}
+        onChange={() =>
+          setStates({
+            ...states,
+            multiselect: { ...states.multiselect, option2: !states.multiselect.option2 },
+          })
+        }
+      />
+      <Select type="checkbox" label="Selected state" checked readOnly />
+      <Select type="checkbox" label="Disabled state" disabled />
+      <H2>Dropdowns</H2>
+      <H3>Normal dropdown</H3>
+      <Dropdown
+        options={options}
+        placeholder={'I am a placeholder'}
+        isSearchable={false}
+        onChange={inputValue => console.log(inputValue)}
+        isValid
+      />
+      <H3>Searchable dropdown</H3>
+      <Dropdown
+        options={options}
+        placeholder={'im tired'}
+        isSearchable
+        formatCreateLabel={inputValue => `Cant find this!!! Use "${inputValue}" instead`}
+        onChange={inputValue => console.log(inputValue)}
+        emptySearchDefaultOption={'Start typing to search'}
+        noOptionsMessage={() => 'u messed up'}
+        canCreateNewOption={false}
+        isValid
+      />
+      <H3>Searchable and creatable dropdown</H3>
+      <Dropdown
+        options={options}
+        placeholder={'Hi I am a placeholder'}
+        isSearchable
+        formatCreateLabel={inputValue => `Cant find this!!! Use "${inputValue}" instead`}
+        onChange={inputValue => console.log(inputValue)}
+        emptySearchDefaultOption={'Start typing to search'}
+        noOptionsMessage={() => 'u messed up'}
+        canCreateNewOption
+        isValid
+      />
+      <H3>Invalid dropdown</H3>
+      <Dropdown
+        options={options}
+        placeholder={'im tired'}
+        isSearchable
+        formatCreateLabel={inputValue => `Cant find this!!! Use "${inputValue}" instead`}
+        onChange={inputValue => console.log(inputValue)}
+        emptySearchDefaultOption={'Start typing to search'}
+        noOptionsMessage={() => 'u messed up'}
+        canCreateNewOption={false}
+        isValid={false}
+        errorMessage={'Please select something!'}
+      />
+      <H3>Debounced dropdown</H3>
+      <Dropdown
+        options={options}
+        placeholder={'im tired'}
+        isSearchable
+        formatCreateLabel={inputValue => `Cant find this!!! Use "${inputValue}" instead`}
+        onChange={inputValue => console.log(inputValue)}
+        emptySearchDefaultOption={'Start typing to search'}
+        noOptionsMessage={() => 'u messed up'}
+        canCreateNewOption={false}
+        isValid
+        errorMessage={'Please select something!'}
+        debounceEnabled
+        throttleTime={1000}
+      />
+      <H3>Debounced creatable dropdown</H3>
+      <Dropdown
+        options={options}
+        placeholder={'im tired'}
+        isSearchable={true}
+        formatCreateLabel={inputValue => `Cant find this!!! Use "${inputValue}" instead`}
+        onChange={inputValue => console.log(inputValue)}
+        emptySearchDefaultOption={'Start typing to search'}
+        noOptionsMessage={() => 'u messed up'}
+        canCreateNewOption
+        isValid
+        errorMessage={'Please select something!'}
+        debounceEnabled
+        throttleTime={1000}
+      />
+      <H2>Change Progress Bar</H2>
+      <Button width="flex" onClick={() => progress < 100 && setProgress(progress + 10)}>
+        Increase!
+      </Button>
+      <Button width="flex" onClick={() => progress >= 10 && setProgress(progress - 10)}>
+        Decrease!
+      </Button>
+      <VerticalProgressBar percent={progress} />
     </>
   )
 }
