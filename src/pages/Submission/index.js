3ce3afe16f45f94cefc0037e662aa10bdfa99f43
Ian <ianmmah@gmail.com>
2020-11-28 16:11:57 -0800

    Get feedback data from firebase

--- src/pages/Submission/index.js
+++ src/pages/Submission/index.js
@@ -7,19 +7,8 @@ const USER_ID = 'aIwA36q0kOw7rDDlCkB2'
 
 export default () => {
   const [isSubmissionsOpen, setIsSubmissionsOpen] = useState(false)
-
-  // TODO: use firebase data
-  // eslint-disable-next-line no-unused-vars
   const [project, setProject] = useState()
-
-  console.log(project)
-
-  // TODO: use firebase data
-  // eslint-disable-next-line no-unused-vars
-  const [feedback, setFeedback] = useState([
-    'Very good!',
-    'Wow this project changed my life like how is this real omg i would give my life for this.',
-  ])
+  const [feedback, setFeedback] = useState([])
 
   useEffect(() => {
     ;(async () => {
@@ -30,7 +19,13 @@ export default () => {
       const youtubeID = new URL(projectData.youtubeUrl).searchParams.get('v')
       projectData.imgUrl = `https://img.youtube.com/vi/${youtubeID}/maxresdefault.jpg`
       projectData.href = projectData.devpostUrl
+      const grades = await team.data().project.collection('Grades').get()
+      const feedback = grades.docs.map(doc => {
+        const docData = doc.data()
+        return docData.notes
+      })
       setProject(projectData)
+      setFeedback(feedback)
     })()
   }, [setProject])
 
