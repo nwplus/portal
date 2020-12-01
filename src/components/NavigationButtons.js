d7e3451440a996aacedbbd0266905cdf115c589a
Kevin Zou <kevin.zou@opencare.com>
2020-12-01 09:19:34 -0800

    Fixup

--- src/components/NavigationButtons.js
+++ src/components/NavigationButtons.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Button } from './Button'
+import { Button } from './Input'
 import { I } from './Typography'
 import { SCREEN_BREAKPOINTS } from '../utility/Constants'
 
