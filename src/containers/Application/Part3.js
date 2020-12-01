9192d70b329c969d0830b4ef1d3a65dbb00d6e22
Kevin Zou <thewebproducer@gmail.com>
2020-11-30 22:08:28 -0800

    Updating styles

--- src/containers/Application/Part3.js
+++ src/containers/Application/Part3.js
@@ -1,15 +1,28 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
 import { Dropdown, Select, TextInput } from '../../components/Input'
 import { H1, QuestionHeading } from '../../components/Typography'
 
+const StyledDropdown = styled(Dropdown)`
+  margin: 1em 0;
+`
+
+const QuestionContainer = styled.div`
+  margin-bottom: 4em;
+`
+
+const StyledTextInput = styled(TextInput)`
+  margin: 0.5em 0 0;
+`
+
 // form part 3
-export default ({ children }) => {
+export default () => {
   const [selectedOption, setSelectedOption] = useState('')
   const [otherOption, setOtherOption] = useState('')
   const [showOther, setShowOther] = useState('')
   const [selectStates, setSelectStates] = useState({
     multiselect: {
-      option1: false, // LHD/Hack Camp
+      option1: false, // LHD / Hack Camp
       option2: false, // nwHacks
       option3: false, // cmd-f
       option4: false, // cmd-f Phases
@@ -30,9 +43,9 @@ export default ({ children }) => {
   ]
 
   const handleSelect = inputValue => {
-    // Show other option if value is 8
     setSelectedOption(inputValue.label)
 
+    // Show additional input for 'Other'
     if (inputValue.label === 'Other') {
       setShowOther(true)
     } else {
@@ -41,110 +54,113 @@ export default ({ children }) => {
   }
 
   return (
-    <div>
-      {children}
-      <QuestionHeading>Question 14</QuestionHeading>
-      <H1>How did you hear about nwHacks?</H1>
-      <Dropdown
-        options={options}
-        placeholder={'Select an option'}
-        isSearchable={false}
-        onChange={inputValue => handleSelect(inputValue)}
-        isValid
-      />
-      {showOther && (
-        <div>
-          <div>Please specify</div>
-          <TextInput value={otherOption} onChange={e => setOtherOption(e.target.value)} />
-        </div>
-      )}
+    <>
+      <QuestionContainer>
+        <QuestionHeading>Question 14</QuestionHeading>
+        <H1>How did you hear about nwHacks?</H1>
+        <StyledDropdown
+          options={options}
+          placeholder={'Select an option'}
+          isSearchable={false}
+          onChange={inputValue => handleSelect(inputValue)}
+          isValid
+        />
+        {showOther && (
+          <div>
+            <div>Please specify</div>
+            <StyledTextInput value={otherOption} onChange={e => setOtherOption(e.target.value)} />
+          </div>
+        )}
+      </QuestionContainer>
 
-      <QuestionHeading>Question 15</QuestionHeading>
-      <H1>Which nwPlus events have you been to? (Select all that apply)</H1>
-      <Select
-        type="checkbox"
-        label="Local Hack Day / Hack Camp"
-        checked={selectStates.multiselect.option1}
-        onChange={() =>
-          setSelectStates({
-            ...selectStates,
-            multiselect: {
-              ...selectStates.multiselect,
-              option1: !selectStates.multiselect.option1,
-            },
-          })
-        }
-      />
-      <Select
-        type="checkbox"
-        label="nwHacks"
-        checked={selectStates.multiselect.option2}
-        onChange={() =>
-          setSelectStates({
-            ...selectStates,
-            multiselect: {
-              ...selectStates.multiselect,
-              option2: !selectStates.multiselect.option2,
-            },
-          })
-        }
-      />
-      <Select
-        type="checkbox"
-        label="cmd-f"
-        checked={selectStates.multiselect.option3}
-        onChange={() =>
-          setSelectStates({
-            ...selectStates,
-            multiselect: {
-              ...selectStates.multiselect,
-              option3: !selectStates.multiselect.option3,
-            },
-          })
-        }
-      />
-      <Select
-        type="checkbox"
-        label="cmd-f Phases"
-        checked={selectStates.multiselect.option4}
-        onChange={() =>
-          setSelectStates({
-            ...selectStates,
-            multiselect: {
-              ...selectStates.multiselect,
-              option4: !selectStates.multiselect.option4,
-            },
-          })
-        }
-      />
-      <Select
-        type="checkbox"
-        label="nwPlus Workshop Series"
-        checked={selectStates.multiselect.option5}
-        onChange={() =>
-          setSelectStates({
-            ...selectStates,
-            multiselect: {
-              ...selectStates.multiselect,
-              option5: !selectStates.multiselect.option5,
-            },
-          })
-        }
-      />
-      <Select
-        type="checkbox"
-        label="nwPlus Boothing"
-        checked={selectStates.multiselect.option6}
-        onChange={() =>
-          setSelectStates({
-            ...selectStates,
-            multiselect: {
-              ...selectStates.multiselect,
-              option6: !selectStates.multiselect.option6,
-            },
-          })
-        }
-      />
-    </div>
+      <QuestionContainer>
+        <QuestionHeading>Question 15</QuestionHeading>
+        <H1>Which nwPlus events have you been to? (Select all that apply)</H1>
+        <Select
+          type="checkbox"
+          label="Local Hack Day / Hack Camp"
+          checked={selectStates.multiselect.option1}
+          onChange={() =>
+            setSelectStates({
+              ...selectStates,
+              multiselect: {
+                ...selectStates.multiselect,
+                option1: !selectStates.multiselect.option1,
+              },
+            })
+          }
+        />
+        <Select
+          type="checkbox"
+          label="nwHacks"
+          checked={selectStates.multiselect.option2}
+          onChange={() =>
+            setSelectStates({
+              ...selectStates,
+              multiselect: {
+                ...selectStates.multiselect,
+                option2: !selectStates.multiselect.option2,
+              },
+            })
+          }
+        />
+        <Select
+          type="checkbox"
+          label="cmd-f"
+          checked={selectStates.multiselect.option3}
+          onChange={() =>
+            setSelectStates({
+              ...selectStates,
+              multiselect: {
+                ...selectStates.multiselect,
+                option3: !selectStates.multiselect.option3,
+              },
+            })
+          }
+        />
+        <Select
+          type="checkbox"
+          label="cmd-f Phases"
+          checked={selectStates.multiselect.option4}
+          onChange={() =>
+            setSelectStates({
+              ...selectStates,
+              multiselect: {
+                ...selectStates.multiselect,
+                option4: !selectStates.multiselect.option4,
+              },
+            })
+          }
+        />
+        <Select
+          type="checkbox"
+          label="nwPlus Workshop Series"
+          checked={selectStates.multiselect.option5}
+          onChange={() =>
+            setSelectStates({
+              ...selectStates,
+              multiselect: {
+                ...selectStates.multiselect,
+                option5: !selectStates.multiselect.option5,
+              },
+            })
+          }
+        />
+        <Select
+          type="checkbox"
+          label="nwPlus Boothing"
+          checked={selectStates.multiselect.option6}
+          onChange={() =>
+            setSelectStates({
+              ...selectStates,
+              multiselect: {
+                ...selectStates.multiselect,
+                option6: !selectStates.multiselect.option6,
+              },
+            })
+          }
+        />
+      </QuestionContainer>
+    </>
   )
 }
