0c227f8e9a0e46597752d6a11aafa65cb32a0631
Kevin Zou <thewebproducer@gmail.com>
2020-11-30 22:10:39 -0800

    Whoops this isn't supposed to be here

--- src/pages/Application/index.js
+++ src/pages/Application/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import Part3 from '../../containers/Application/Part3'
 
 // applicant dashboard
 export default () => {
   return (
     <div>
       <h1>Welcome back, hacker!</h1>
-      <Part3 />
     </div>
   )
 }
