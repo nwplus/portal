2e4154a643d796383a456c3b35f967c48fda813e
Jenny Pan <panjenny0@gmail.com>
2020-11-30 17:41:55 -0800

    linting errors angrycry

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { P } from './Typography'
 import icon from '../assets/nwhacks_icon.svg'
-import Button from './Button'
+import { Button } from './Input/Button'
 
 const NavContainer = styled.div`
   direction: rtl;
