8ea76a10c3613064b931f03e1fafee5f0faee16d
Jenny Pan <panjenny0@gmail.com>
2020-12-02 01:05:11 -0800

    centered spinner

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,24 +1,14 @@
 import React from 'react'
-import styled from 'styled-components'
 import { css } from '@emotion/core'
 import MoonLoader from 'react-spinners/MoonLoader'
-import logo from '../assets/logo.svg'
 
-const LoadingContainer = styled.div`
+const override = css`
   position: fixed;
   top: 50%;
   left: 50%;
-  z-index: 200;
-`
-const override = css`
-  display: block;
-  margin: 0 auto;
-  position: relative;
-  z-index: 200;
+  background-size: 100%;
 `
 
 export default ({ loading }) => (
-  <LoadingContainer>
-    <MoonLoader css={override} color="#fff" size={80} loading={loading} />
-  </LoadingContainer>
+  <MoonLoader css={override} color="#fff" size={80} loading={loading} />
 )
