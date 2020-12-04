050e19e6449e6544aa289d798a9034cc21c80592
jackyzha0 <j.zhao2k19@gmail.com>
2020-12-03 19:57:33 -0800

    fix banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,15 +9,18 @@ const bannerWidth = {
 
 const StyledContainer = styled.div`
   background: ${p => p.theme.colors.banner};
-  backdrop-filter:${p => (!!p.blur ? 'blur(15px)' : 'none')}
+  backdrop-filter: ${p => (!!p.blur ? 'blur(15px)' : 'none')};
   border-radius: 4px;
   padding: 48px;
   ${CenterHorizontally}
   width: ${p => (p.wide ? bannerWidth.wide : bannerWidth.default)};
 `
 
-export default ({ className, wide, blur, children }) => (
-  <StyledContainer className={className} wide={wide} blur={blur}>
-    {children}
-  </StyledContainer>
-)
+export default ({ className, wide, blur, children }) => {
+  console.log(blur)
+  return (
+    <StyledContainer className={className} wide={wide} blur={blur}>
+      {children}
+    </StyledContainer>
+  )
+}
