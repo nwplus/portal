64d05b234138979ac0bad7c339bcde99aa576bd9
Giulio Rossi <ciuffi9@mac.com>
2020-12-03 16:09:25 -0800

    initialize analytics and new app event

--- src/utility/firebase.js
+++ src/utility/firebase.js
@@ -1,10 +1,12 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import 'firebase/analytics'
 import {
   hackerApplicationTemplate,
   applicantStatus,
   DB_COLLECTION,
   DB_HACKATHON,
+  AnalyticsEvents,
 } from '../utility/Constants'
 import { formatProject } from './utilities'
 
@@ -14,7 +16,7 @@ if (!firebase.apps.length) {
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
@@ -24,6 +26,8 @@ if (!firebase.apps.length) {
 
 export const firestore = firebase.firestore
 export const db = firebase.firestore()
+
+export const analytics = firebase.analytics()
 
 export const livesiteDocRef = db.collection('InternalWebsites').doc('Livesite')
 export const applicantsRef = db.collection(DB_COLLECTION).doc(DB_HACKATHON).collection('Applicants')
@@ -98,6 +102,7 @@ export const getProject = async (user_id, setProjectCallback, setFeedbackCallbac
 }
 
 const createNewApplication = async user => {
+  analytics.logEvent(AnalyticsEvents.signup, { userId: user.uid })
   const userId = {
     _id: user.uid,
   }
