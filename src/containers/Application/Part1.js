c36be2edc2ef98e5d9ef1e6d69d932c54e3caf7c
Giulio Rossi <Ciuffi9@mac.com>
2020-12-04 20:30:42 -0800

    ESLINT !!!

--- src/containers/Application/Part1.js
+++ src/containers/Application/Part1.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useLocation } from 'wouter'
 import BasicInfo from '../../components/ApplicationForm/BasicInfo'
 import { useHackerApplication } from '../../utility/HackerApplicationContext'
@@ -17,7 +17,9 @@ export default () => {
   /**
    * Saves and moves to next page
    * TODO I need a button !
+   * TODO remove this eslint thing once this is used
    */
+  // eslint-disable-next-line no-unused-vars
   const nextPage = async () => {
     await forceSave()
     setLocation('/application/part-2')
