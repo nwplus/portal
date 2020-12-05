c5443c9d184bc40a6a46b58e1014f8bfc4d230bc
Giulio Rossi <ciuffi9@mac.com>
2020-12-04 22:06:02 -0800

    show message

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -15,6 +15,8 @@ const ErrorMessage = message => (
   <>
     There was an issue logging you in. If this persists, please contact"
     <A href="mailto:info@nwplus.io">info@nwplus.io</A>.
+    <br />
+    {message}
   </>
 )
 
