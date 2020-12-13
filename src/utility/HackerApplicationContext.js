f3fedf35c7ea2940e5fd19658057207962a916af
Jenny Pan <panjenny0@gmail.com>
2020-12-12 22:09:33 -0800

    added null check for force save

--- src/utility/HackerApplicationContext.js
+++ src/utility/HackerApplicationContext.js
@@ -30,7 +30,7 @@ export function HackerApplicationProvider({ children }) {
   /**Saves the users application, can be called manually or through interval */
   /**Uses a reference to the application because I don't want all my useEffects triggering every time someone changes the application. */
   const forceSave = useCallback(async () => {
-    if (!user) return
+    if (!user || !applicationRef) return
     const updatedApp = {
       ...applicationRef.current,
       submission: {
