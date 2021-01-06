9833d3994877dbc8d5dd1235bb19472e53885ce6
Jenny Pan <panjenny0@gmail.com>
2021-01-05 19:34:53 -0800

    lint

--- src/pages/Application/Closed.js
+++ src/pages/Application/Closed.js
@@ -2,26 +2,21 @@ import React from 'react'
 import Landing from '../../containers/Landing'
 import { A } from '../../components/Typography'
 import { SOCIAL_LINKS } from '../../utility/Constants'
-import { Button } from '../../components/Input'
-import { ButtonContainer } from '../Login'
-import { useLocation } from 'wouter'
 
-export default () => {
-  return (
-    <Landing
-      heading="Thanks for your interest in nwHacks 2021"
-      description={
-        <>
-          We are no longer accepting applications, but we do hope to see you at our future events!
-          Visit our site{' '}
-          <A bolded color="primary" href={SOCIAL_LINKS.WEBSITE}>
-            nwplus.io
-          </A>{' '}
-          or follow us on social media to learn about our events and other ways to engage with the
-          technology community!
-        </>
-      }
-      showFooter
-    />
-  )
-}
+export default () => (
+  <Landing
+    heading="Thanks for your interest in nwHacks 2021"
+    description={
+      <>
+        We are no longer accepting applications, but we do hope to see you at our future events!
+        Visit our site{' '}
+        <A bolded color="primary" href={SOCIAL_LINKS.WEBSITE}>
+          nwplus.io
+        </A>{' '}
+        or follow us on social media to learn about our events and other ways to engage with the
+        technology community!
+      </>
+    }
+    showFooter
+  />
+)
--- src/utility/HackerApplicationContext.js
+++ src/utility/HackerApplicationContext.js
@@ -149,7 +149,7 @@ export function HackerApplicationProvider({ children }) {
    */
   return applicationOpen === null || application === undefined ? (
     <Spinner />
-  ) : !applicationOpen && window.location.pathname != '/application' ? (
+  ) : !applicationOpen && window.location.pathname !== '/application' ? (
     <Closed />
   ) : (
     <HackerApplicationContext.Provider value={{ application, updateApplication, forceSave }}>
