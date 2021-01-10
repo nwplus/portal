2cb66d663ce926299c13a83fc93fbbbecbb8b59f
jackyzha0 <j.zhao2k19@gmail.com>
2021-01-09 16:51:39 -0800

    judging percentage fix

--- src/pages/Submission.js
+++ src/pages/Submission.js
@@ -22,7 +22,7 @@ export default () => {
 
   const getProject = async () => {
     const d = await getUserApplication(user.uid)
-    if (d.submittedProject === '') {
+    if (!d.submittedProject) {
       setSubmission(false)
       return
     }
