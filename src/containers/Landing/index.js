9110e15a07160989cf20e68900ca21101044a6f5
Jae Wu <jaewuchun@gmail.com>
2021-12-01 14:25:32 -0800

    logo resize doesn't hide login

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -36,14 +36,17 @@ const StyledLogoLockup = styled.img`
   ${p => p.theme.mediaQueries.tabletLarge} {
     top: 18%;
     width: 80%;
+    ${p => p.theme.name !== 'nwPlus' && `top: 15%; width: 20%;`}
   }
   ${p => p.theme.mediaQueries.tablet} {
     top: 15%;
     width: 82%;
+    ${p => p.theme.name !== 'nwPlus' && `top:22%; width: 20%;`}
   }
   ${p => p.theme.mediaQueries.xs} {
     width: 90%;
     top: 19%;
+    ${p => p.theme.name !== 'nwPlus' && `top: 15%; width: 35%;`}
   }
 `
 
