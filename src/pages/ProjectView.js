51882b2727e4229c2c6e7096b5d527890ae2ef59
Your Name <new2282307660@gmail.com>
2022-01-04 22:34:25 -0800

    plopping down all the properties of the object

--- src/pages/ProjectView.js
+++ src/pages/ProjectView.js
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
+import { H1, P } from '../components/Typography'
+
 import { getSubmission } from '../utility/firebase'
+import { MAX_CHARACTERS_IN_DESCRIPTION } from '../utility/Constants'
+import { cutString } from '../utility/utilities'
 import { Loading } from '../components/HeroPage'
 import Youtube from '../components/Youtube'
+
+const StyledProjectContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 const StyledYoutube = styled(Youtube)`
   width: 600px;
@@ -13,9 +23,14 @@ const StyledYoutube = styled(Youtube)`
 
 const Project = ({ project }) => {
   return (
-    <div>
+    <StyledProjectContainer>
+      <H1>{project.title}</H1>
       <StyledYoutube src={project.youtubeUrl} />
-    </div>
+      <P>{cutString(project.shortDescription, MAX_CHARACTERS_IN_DESCRIPTION)}</P>
+      <p>{project.longDescription}</p>
+      <p>{project.teamMembers}</p>
+      <p>{project.links}</p>
+    </StyledProjectContainer>
   )
 }
 
