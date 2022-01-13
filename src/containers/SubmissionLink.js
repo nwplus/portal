0dc4a25335166f3d092c9ce7c669c295683f4e21
Vincent Chiang <vincentchiang1@gmail.com>
2022-01-12 22:27:05 -0800

    add validation

--- src/containers/SubmissionLink.js
+++ src/containers/SubmissionLink.js
@@ -71,7 +71,17 @@ export default ({ user, refreshCallback }) => {
             console.log(member.email)
             const res = await applicantsRef.where('basicInfo.email', '==', member.email).get()
             if (res.docs.length > 0) {
-              return await applicantsRef.doc(res.docs[0].id).update({ submittedProject: projectId })
+              const { applicationStatus, attending, responded } = res.docs[0].data().status
+              if (applicationStatus !== 'accepted' || !attending || !responded) {
+                setError(new Error(member.email + ' is not a valid hacker.'))
+              } else if (res.docs[0].data().submittedProject) {
+                setError(
+                  new Error(member.email + ' is already part of a different project submission.')
+                )
+              } else
+                return await applicantsRef
+                  .doc(res.docs[0].id)
+                  .update({ submittedProject: projectId })
             }
           })
         )
@@ -80,7 +90,7 @@ export default ({ user, refreshCallback }) => {
       }
     } else {
       try {
-        const res = await projectsRef.add(projectSubmission)
+        const project = await projectsRef.add(projectSubmission)
         // await applicantsRef.doc(user.uid).update({ submittedProject: res.id })
         await Promise.all(
           // TODO: Check that the person doesn't already have a project
@@ -89,7 +99,17 @@ export default ({ user, refreshCallback }) => {
             console.log(member.email)
             const res = await applicantsRef.where('basicInfo.email', '==', member.email).get()
             if (res.docs.length > 0) {
-              return await applicantsRef.doc(res.docs[0].id).update({ submittedProject: res.id })
+              const { applicationStatus, attending, responded } = res.docs[0].data().status
+              if (applicationStatus !== 'accepted' || !attending || !responded) {
+                setError(new Error(member.email + ' is not a valid hacker.'))
+              } else if (res.docs[0].data().submittedProject) {
+                setError(
+                  new Error(member.email + ' is already part of a different project submission.')
+                )
+              } else
+                return await applicantsRef
+                  .doc(res.docs[0].id)
+                  .update({ submittedProject: project.id })
             }
           })
         )
