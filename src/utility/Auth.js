24bf236d313f1eb2f25d88b2e4490813f004296a
Jae Wu <jaewuchun@gmail.com>
2023-01-02 14:49:21 -0800

    removed alerts

--- src/utility/Auth.js
+++ src/utility/Auth.js
@@ -59,7 +59,7 @@ export function AuthProvider({ children }) {
 const handleUser = async (setUser, setLocation) => {
   const user = firebase.auth().currentUser
   const { redirect, status } = await getUserStatus(user)
-  alert(redirect, status)
+  // alert(redirect, status)
   user.status = status
   user.redirect = redirect
   const admin = await checkAdminClaim(user)
@@ -85,7 +85,7 @@ export const getRedirectUrl = redirect => {
 }
 
 export const handleRedirect = async (redirect, setLocationCallback) => {
-  alert(redirect, getRedirectUrl(redirect))
+  // alert(redirect, getRedirectUrl(redirect))
   const submissionOpen = (await livesiteDocRef.get()).data().submissionsOpen
   setLocationCallback(submissionOpen ? '/submission' : getRedirectUrl(redirect))
 }
