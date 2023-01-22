883402e5cf22b03740b45a3a92eb884842482b73
Jae Wu <jaewuchun@gmail.com>
2023-01-22 12:55:49 -0800

    added table numbers

--- src/pages/ProjectView.js
+++ src/pages/ProjectView.js
@@ -52,6 +52,13 @@ const StyledH1 = styled(H1)`
   margin-bottom: 16px;
 `
 
+const StyledA = styled.a`
+  text-decoration: underline;
+  color: white;
+  font-weight: bold;
+  margin-bottom: 21px;
+`
+
 const StyledP = styled(P)`
   color: inherit;
   margin-bottom: 21px;
@@ -89,11 +96,159 @@ const Project = ({ project }) => {
     }
   }
 
+  const arr = [
+    '',
+    `Peer2Pair`,
+    `Post-That`,
+    `Slingo`,
+    `UBC Course Map`,
+    `MovifyGames`,
+    `UBOntime`,
+    `Cooking on the Fly`,
+    `// TODO: Tasks, Objectives, and Discussions Organized`,
+    `Wear3Words`,
+    `codeStreaks`,
+    `UnDUE`,
+    `Mode`,
+    `Intelliphant`,
+    `Chainlearn`,
+    `sobriety.io`,
+    `Moments`,
+    `whisper`,
+    `Stride`,
+    `Build-A-Jous`,
+    `Foodle`,
+    `FOOD4MOOD`,
+    `Complexion.ai`,
+    `Check`,
+    `classyfi`,
+    `React Todo-List`,
+    `Maya, Mental Health Assistant`,
+    `Yudo`,
+    `SayWhatever`,
+    `Computer Science Forums - CSF`,
+    `IMGUESSR.io`,
+    `kizuna 絆`,
+    `Aniprofinder`,
+    `Text Reminder`,
+    `PeerSafe`,
+    `StarStruck`,
+    `Pill Dispenser (PigeonPill)`,
+    `Blinkr: Smart Eye Protection`,
+    `VirtualPrep`,
+    `TidBit`,
+    `VR Speech Simulator`,
+    `RateABox`,
+    `Visual.ai.d`,
+    `Future Store`,
+    `Venture Bot`,
+    `Reworkd.ai`,
+    `TutorShare`,
+    `Incode`,
+    `sabr`,
+    `LATER (Listen And Transcribe Easily and Rapidly)`,
+    `Text Memoirs`,
+    `WatchLog`,
+    `PLANTIS`,
+    `Xplore`,
+    `Social Diary`,
+    `noteAI Chrome Extension`,
+    `Tabby Pals`,
+    `peerAdvice`,
+    `playc3`,
+    `I Wet My Plants`,
+    `Food Share`,
+    `reCap`,
+    `GroPay`,
+    `Where2Eat`,
+    `Quiz Flash`,
+    `BuckIt`,
+    `Propertization`,
+    `Covinox- Where protection meets privacy`,
+    `Make Your Cyber Friend`,
+    `Kalsi Speakers`,
+    `Beeno`,
+    `Artificial Closet`,
+    `Adventure Awaits`,
+    `Mood Tracker and Mental Health Journal (Prototype)`,
+    `Visto`,
+    `Mindful OutLook`,
+    `CaptionConcierge`,
+    `Sync Up`,
+    `HackaTeam`,
+    `EatRight`,
+    `Hungry Husky`,
+    `Career Roaster`,
+    `dont kill our plant`,
+    `UniTalkCA`,
+    `Plants Live Here`,
+    `MediScribe`,
+    `Scheduler`,
+    `Lightweight`,
+    `CarPoolPal`,
+    `Recall`,
+    `EcoSnap`,
+    `compost`,
+    `MEDiator`,
+    `GitGoin'`,
+    `BludBud`,
+    `Orbi.`,
+    `Verif.AI`,
+    `SmartSlides`,
+    `Workout Woulette`,
+    `prescript`,
+    `uniMatch`,
+    `Flash`,
+    `Pixel Perfect Match`,
+    `mindfulU`,
+    `The Sky's the Limit`,
+    `UBC Course Scheduler`,
+    `Survival Guide`,
+    `WhereU@`,
+    `GradLink`,
+    `Chit Chat`,
+    `journo: AI-Powered Journaling App`,
+    `! What The Food`,
+    `sprout.fyi`,
+    `Time2Eat`,
+    `Smart Foods`,
+    `Missing Mystery`,
+    `Maxiclip`,
+    `R2C - Recommend 2 Friend`,
+    `UBC NAVI-BOT`,
+    `Who's That Birdy-mon?`,
+    '',
+    `UBC SOCIAL LIFE`,
+    '',
+    `Speech Analytics`,
+    `Recycle it`,
+    `Sleepy Ninja!`,
+    `Live Sport Scores`,
+    `Presently`,
+    `Cloud Optimization Platform`,
+    `Sipwise Web Application`,
+    `Classic 9 Ball Game`,
+    `KidsPedia: Unleashing the Power of Education through Breaking Barriers`,
+    `Project Panini`,
+    `Slice of Life`,
+  ]
+
   return (
     <StyledProjectContainer>
       <StyledBanner>
         <StyledH1>{project.title}</StyledH1>
         <StyledP>Created By: {teamMembers.join(' | ')}</StyledP>
+        {arr.indexOf(project.title) !== -1 ? (
+          <StyledP>{arr.indexOf(project.title)}</StyledP>
+        ) : (
+          <StyledA
+            href="https://docs.google.com/spreadsheets/d/1ZuFZ6tSdY-c8Iui_CH99hZDqbt_K-Aqf5f6Pmho08fU"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            View project seating table numbers
+          </StyledA>
+        )}
       </StyledBanner>
       {/* <StyledYoutube src={project.links.youtube} /> */}
       <StyledDiv>
