4cac22909ba2eac311343b7296df2b81787256b2
DonaldKLee <Donald3010@outlook.com>
2023-11-13 01:53:30 -0800

    made background component

---
+++ src/components/BackgroundImage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import hackcamp2023bg from '../assets/hc2023mainbg.png'
+
+const Hackcamp2023Background = styled.div`
+  position: fixed;
+  background-image: URL(${hackcamp2023bg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-width: 100%;
+  min-height: 100%;
+  z-index: -1;
+  top: 0;
+  left: 270px;
+`
+
+const Hackcamp2023BG = () => {
+  return <Hackcamp2023Background />
+}
+
+export default Hackcamp2023BG
