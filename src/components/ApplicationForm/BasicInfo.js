dd50160f7dc54f338f734104dab07d5e22df55b1
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2023-11-29 18:03:49 -0800

    Add 2023 grad year option

--- src/components/ApplicationForm/BasicInfo.js
+++ src/components/ApplicationForm/BasicInfo.js
@@ -60,6 +60,7 @@ const identifyAsUnderrepresentedOptions = [
 ]
 
 const graduationOptions = [
+  { value: 2023, label: '2023' },
   { value: 2024, label: '2024' },
   { value: 2025, label: '2025' },
   { value: 2026, label: '2026' },
