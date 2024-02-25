5f338246fdb05ad1b995cd6f5645bd706a240127
samtuhseng <samantha.tseng90@gmail.com>
2024-02-25 15:11:41 -0800

    small fix

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -1,6 +1,5 @@
 import { createGlobalStyle } from 'styled-components'
 import './fonts/style.css'
-import { backgroundImage } from '../assets/cmdf_homebg.svg'
 
 export default createGlobalStyle`
   body {
