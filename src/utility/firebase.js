20ffda3232c225c9f776c865fec559b3301474cc
martincai8 <martincai46@gmail.com>
2024-08-03 11:51:48 -0700

    Change livesite doc name to Portal

--- src/utility/firebase.js
+++ src/utility/firebase.js
@@ -30,7 +30,7 @@ export const storage = firebase.storage()
 
 export const analytics = firebase.analytics()
 
-export const livesiteDocRef = db.collection('InternalWebsites').doc('LivesiteV2')
+export const livesiteDocRef = db.collection('InternalWebsites').doc('Portal')
 export const currentHackathonRef = dbHackathonName => {
   return db.collection(DB_COLLECTION).doc(dbHackathonName)
 }
