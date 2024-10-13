37b9e8b58f15758c97a8899ff9cc7789835df396
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-10-13 00:01:47 -0700

    Make attended events card dynamic

--- src/components/Rewards/AttendedEventsCard.jsx
+++ src/components/Rewards/AttendedEventsCard.jsx
@@ -1,6 +1,5 @@
-import styled, { useTheme } from 'styled-components'
+import styled from 'styled-components'
 import { StyledSVG } from '../Schedule/Tag'
-import { EVENT_TYPES } from '../Schedule/Constants'
 import { P } from '../Typography'
 import Icon from '../../assets/checkmark.svg?react'
 
@@ -12,13 +11,14 @@ const AttendedEventsCardContainer = styled.div`
   color: ${p => p.theme.colors.cardText};
   background: ${p => p.theme.colors.backgroundTertiary};
   border-radius: 5px;
-  padding: 1em 1.5em;
+  padding: 0.875em 1em;
 `
 
 const EventDetailsContainer = styled.div`
   flex-grow: 1;
   display: flex;
   justify-content: space-between;
+  gap: 2em;
   align-items: center;
 `
 
@@ -55,22 +55,28 @@ const PointsText = styled(P)`
   font-weight: 700;
   color: ${props => props.color};
   font-size: 0.875em;
+  margin: 0;
 `
 
-const AttendedEventsCard = () => {
-  const theme = useTheme()
-  const event_type = Object.entries(EVENT_TYPES(theme)).map(([key, event_type], i) => event_type)[0]
+/**
+ * @typedef {{name: string, time: string, points: number, color: string}} AttendedEventsCardProps
+ */
+
+/**
+ * @param {AttendedEventsCardProps} param0
+ */
+const AttendedEventsCard = ({ name, time, points, color }) => {
   return (
     <AttendedEventsCardContainer>
-      <CheckmarkSVG color={event_type.colour} />
+      <CheckmarkSVG color={color} />
       <EventDetailsContainer>
         <EventDetails>
-          <EventName>Some Event</EventName>
-          <EventTime>10:00AM - 11:00AM</EventTime>
+          <EventName>{name}</EventName>
+          <EventTime>{time}</EventTime>
         </EventDetails>
         <TagPointsContainer>
-          <StyledSVG color={event_type.colour} />
-          <PointsText color={event_type.colour}>600 pts earned</PointsText>
+          <StyledSVG color={color} />
+          <PointsText color={color}>{points} pts earned</PointsText>
         </TagPointsContainer>
       </EventDetailsContainer>
     </AttendedEventsCardContainer>
