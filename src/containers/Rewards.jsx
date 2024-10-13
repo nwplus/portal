a7dfea7a8a80cd5a005b94b2023bb6427ae2ed0b
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-10-12 23:38:04 -0700

    Add placeholder attended events card + basic rewards layout

--- src/containers/Rewards.jsx
+++ src/containers/Rewards.jsx
@@ -1,3 +1,34 @@
-const Rewards = () => {}
+import styled from 'styled-components'
+import AttendedEvents from '../components/Rewards/AttendedEvents'
+
+const RewardsContainer = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+
+  ${p => p.theme.mediaQueries.mobile} {
+    flex-direction: column;
+  }
+`
+
+const RewardsSummaryContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 35%;
+  flex-shrink: 0;
+`
+
+const RewardsContentContainer = styled.div``
+
+const Rewards = () => {
+  return (
+    <RewardsContainer>
+      <RewardsSummaryContainer>
+        <AttendedEvents />
+      </RewardsSummaryContainer>
+      <RewardsContainer></RewardsContainer>
+    </RewardsContainer>
+  )
+}
 
 export default Rewards
