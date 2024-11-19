6374cfd6c34aa18893a26846290b2e569ca4c466
DonaldKLee <Donald3010@outlook.com>
2024-11-18 16:53:00 -0800

    nwhacks theme

--- src/theme/ThemeProvider.jsx
+++ src/theme/ThemeProvider.jsx
@@ -153,6 +153,114 @@ const nwHacksTheme = {
   ...base,
   name: 'nwhacks',
   colors: {
+    background: '#EDDECC',
+    backgroundSecondary: '#E2D0BA',
+    backgroundTertiary: 'linear-gradient(180deg, #F9C745 0%, #FF880F 100%)',
+    text: '#3A2F21',
+    textSecondary: '#01033D',
+    highlight: '#30C55A',
+    warning: '#D04E51',
+    error: '#E03131',
+    scrollbar: '#FFFFFF',
+    required: '#D04E51',
+    link: '#F3BC1C',
+    linkHover: '#75AEE2',
+    lines: '#45171A', // ask design for a variable (for gamification)
+    bar: '#00A4E0', // ask design for a variable (for gamification)
+    barBackground: '#371315', // ask design for a variable (for gamification)
+
+    // sidebar
+    sidebar: {
+      background: 'linear-gradient(191.3deg, #77CFDC -57.93%, #23A2CE 78.18%)',
+      backgroundSecondary: '#EDDECC',
+      textSectionHeader: '#F9C745',
+      textSelected: '#FFFFFF',
+      textDefault: '#EBEBEB',
+      backgroundSelected: '#1A94BC',
+      textHover: '#FFFFFF',
+      backgroundHover: '#A8138C',
+    },
+
+    // buttons
+    button: {
+      primary: {
+        text: '#3A2F21',
+        background: {
+          default: 'linear-gradient(90deg, #F1BC3E 26.5%, #ECB32A 100%)',
+          hover: 'linear-gradient(90deg, #F7D175 0%, #FBC951 100%);',
+          clicked: 'linear-gradient(90deg, #F3BC3A 0%, #DDA419 100%)',
+        },
+      },
+      secondary: {
+        text: '#45171A',
+        background: {
+          default: '#F3C241',
+          hover: '#FFD569', // missing
+          clicked: '#E4B63F', // mising
+        },
+      },
+      warning: {
+        text: '#FFFFFF',
+        background: {
+          default: '#E03131',
+          hover: '#B52828',
+          clicked: '#C92C2C',
+        },
+      },
+      backgroundDisabled: '#C9C2BF',
+    },
+
+    // schedule
+    schedule: {
+      background:
+        'linear-gradient(180deg, rgba(0, 163, 224, 0.6) 0%, rgba(255, 248, 229, 0.6) 99.98%)',
+      mainEventTag: '#3268A5',
+      workshopTag: '#DE0148',
+      activityTag: '#A9158D',
+      lines: '#FFFFFF',
+    },
+
+    // form elements
+    input: {
+      border: '#45171A',
+      hover: '#A9158D',
+      disabled: '#B0B0B066',
+    },
+    select: {
+      border: '#453017',
+      background: {
+        default: 'transparent',
+        hover: '#75AEE2',
+      },
+    },
+    dropdown: {
+      background: {
+        selected: '#D4BEA4',
+        hover: '#E2D0BA',
+      },
+    },
+
+    // auth
+    login: {
+      googleAuthBackground: '#FFFFFF',
+      googleAuthBackgroundHover: '##FB8EAD',
+      googleAuthText: '#2A3C4A',
+      githubAuthText: '#FFFFFF',
+      githubAuthBackground: '#577079',
+      githubAuthBackgroundHover: '#3A9BBE',
+    },
+
+    // misc
+    faq: {
+      toggle: '#45171A',
+    },
+  },
+}
+
+const cmdfTheme = {
+  ...base,
+  name: 'cmd-f',
+  colors: {
     background: '#C8E5F0',
     backgroundSecondary: '#93BEE5',
     backgroundTertiary: 'linear-gradient(180deg, #F9C745 0%, #FF880F 100%)',
@@ -253,86 +361,6 @@ const nwHacksTheme = {
     // misc
     faq: {
       toggle: '#45171A',
-    },
-  },
-}
-
-const cmdfTheme = {
-  ...base,
-  name: 'cmd-f',
-  colors: {
-    background: '#5968A6',
-    card: '#323858', // BG Accent
-    cardText: '#ffffff',
-    border: 'rgba(0, 0, 0, 0.3)',
-    cardSecondary: '#C8BFB6',
-    secondaryBackground: '#323858',
-    // secondaryBackgroundTransparent: '#EBC02910',
-    secondaryBackgroundTransparent: '#F1E9DF',
-    sidebar: {
-      background: '#323858',
-      secondary: '#FFFFFF',
-      link: '#CAD1F7',
-      hover: '#5968A6',
-      primary: '#48FFF4',
-    },
-    foreground: '#FFFFFF',
-    error: '#ff0033',
-    success: '#629F5D',
-    toastText: '#fff', // Color for text in toast messages (Toast.js)
-    warning: '#FF8989',
-    secondaryWarning: '#EF6C6C',
-    primary: '#48FFF4',
-    secondary: '#CAD1F7',
-    primaryGradient: '#82B185',
-    primaryGradientHover: '#CAD1F7',
-    default: '#FFFFFF',
-    highlight: '#192825',
-    text: '#FFFFFF',
-    link: '#3D995F',
-    linkHover: '#CAD1F7',
-    tertiaryHover: '#CAD1F7', // button hover
-    // Styling for schedule page
-    schedule: {
-      background: 'linear-gradient(0deg, #404871 100%, #58618C 80%, #949ABA 20%)',
-      event: 'white',
-      text: '#F0EEF2', //greyscale
-      timestamp: '#48FFF4',
-    },
-    selects: {
-      border: '#CAD1F7',
-      text: '#FFFFFF',
-      hover: '#48FFF4',
-      disabled: 'rgba(0, 0, 0, 0.4)',
-      selected: '#48FFF4',
-      placeholder: 'rgba(0, 0, 0, 0.4)',
-    },
-    button: {
-      // Application form buttons
-      text: '#0A1361', // For primary button
-      background: '#5968A6', // Primary button
-      border: '#9AD4DE',
-      hoverbg: '#CAD1F7',
-      hoverborder: '#A4A9F1',
-      outlineText: '#FFFFFF', // Secondary button
-      outlineBackground: '#5968A6',
-      outlineBackgroundHover: '#48FFF4',
-      outlineBorder: '#CFD6FF',
-      grey500: '#BDBAC3',
-      grey700: '#55525B',
-    },
-    hover: '#A0B9C0',
-    scrollbar: '#5F8CA2',
-    banner: 'transparent',
-    login: {
-      background: '#C0A68B',
-      text: '#fff',
-      googleBg: '#fff',
-      googleText: '#000',
-      googleBgHover: 'rgba(255, 255, 255, 0.8)',
-      githubBg: '#577079',
-      githubText: '#fff',
-      githubBgHover: 'rgba(87, 112, 121, 0.8)',
     },
   },
 }
